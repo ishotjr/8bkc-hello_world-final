2e64507e0bb20d318cee23cc73c781683dd09891
ishotjr <github@ishotjr.com>
2018-07-12 23:26:16 -0400

    :heavy_check_mark: :five: Code Checkpoint 5

--- main/main.c
+++ main/main.c
@@ -3,15 +3,21 @@
 #include "ugui.h" // Full uGUI library - Full uGUI reference guide: http://embeddedlightning.com/download/reference-guide/
 #include "powerbtn_menu.h" // Power Button menu stuff powerbtn_menu_show, constants, etc
 #include "string.h" // Needed for str_len()
+#include "math.h" // Needed for fabs(), fmod(), sin()
 
 char hello[] = "Hello World!"; // Store string that will be used
 
 const UG_FONT *font = &FONT_6X8; // Define font for more readable code
 
+float phase = 0; // This will be used for the sine wave animation
+
 #define SW KC_SCREEN_W // Screen width
 #define SH KC_SCREEN_H // Screen height
 #define CW font->char_width // Character width
 #define CH font->char_height // Character height
+
+#define SIN_FREQ 0.375 // Multiplier for the sine wave density: smaller = larger wave
+#define SIN_DEG 1 // Number of degrees per frame the phase for the sine wave moves
 
 static void do_powerbtn_menu() {
     int pwr_input = powerbtn_menu_show(kcugui_get_fb()); // Call the powerbutton menu
@@ -41,11 +47,15 @@ void app_main() {
         do_powerbtn_menu();
     }
 
+    //phase = fmod(phase + 1 / (2 * PI) * SIN_DEG, 2*PI); // Move phase forward by SIN_DEG degrees per frame
+    // PI s/b M_PI
+    phase = fmod(phase + 1 / (2 * M_PI) * SIN_DEG, 2*M_PI); // Move phase forward by SIN_DEG degrees per frame
+    
     kcugui_cls(); // Clear the display
 	for (int i = 0; i < strlen(hello); i++) {
 		UG_PutChar(hello[i], // Char
 			i * CW + (SW - strlen(hello) * CW) / 2, // X
-			0, // Y
+			round(sin(i * SIN_FREQ + phase) * (SH / 2 - CH / 2) + (SH / 2 - CH / 2)), // Y
 			C_WHITE, // Foreground Color
 			C_BLACK); // Background Color
 	}
